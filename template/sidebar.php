35de183a8342912f9592e581bfff526b37dcce75
Imam Maskuri <90993075+mskur@users.noreply.github.com>
2022-05-17 14:07:35 +0700

    Update sidebar.php

--- template/sidebar.php
+++ template/sidebar.php
@@ -2,6 +2,6 @@
     <center>
         <h4>Perpustakaan<br>Akbar</h4>
     </center>
-    <a href="../dataIndex/indexBuku.php"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
-    <a href="../insert/addBuku.php"><i class="fas fa-cogs"></i><span>Tambah Barang</span></a>
+    <a href="../dataIndex/indexBuku.php"><i class="fas fa-desktop"></i><span>Buku Perpustakaan</span></a>
+    <a href="../insert/addBuku.php"><i class="fas fa-cogs"></i><span>Tambah Buku</span></a>
 </div>
