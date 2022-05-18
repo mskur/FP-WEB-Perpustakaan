699a5e6f1ae07c23ae9c1528dc1066a4b64f4bd6
Imam Maskuri <90993075+mskur@users.noreply.github.com>
2022-05-18 18:26:39 +0700

    Delete sidebar.php

--- template/sidebar.php
+++
@@ -1,7 +0,0 @@
-<div class="sidebar">
-    <center>
-        <h4>Perpustakaan<br>Akbar</h4>
-    </center>
-    <a href="../dataIndex/indexBuku.php"><i class="fas fa-desktop"></i><span>Buku Perpustakaan</span></a>
-    <a href="../insert/addBuku.php"><i class="fas fa-cogs"></i><span>Tambah Buku</span></a>
-</div>
