1699e5d6f33ea0ac192f24e99ebd3f210b4018fd
Imam Maskuri <90993075+mskur@users.noreply.github.com>
2022-05-18 22:01:49 +0700

    Update search.php

--- script/search.php
+++ script/search.php
@@ -16,47 +16,53 @@ $buku = query("SELECT * FROM buku
 ?>
 
 
-<?php if($buku == null): ?>
+<?php if ($buku == null) : ?>
     <h1 style="text-align: center; ">Data tidak ditemukan</h1>
 
-<?php else: ?>
+<?php else : ?>
     <table border="2" cellpadding="3" cellspacing="1">
         <thead>
             <tr>
                 <th>Kode Buku</th>
-                <th>Nama Buku</th>
+                <th>Judul</th>
                 <th>Tahun</th>
                 <th>Penulis</th>
                 <th>Rak</th>
                 <th>Genre</th>
                 <th>Status</th>
+                <th>Barcode</th>
                 <th>Update</th>
                 <th>Delete</th>
             </tr>
         </thead>
         <tbody>
-            <?php foreach($buku as $bk) : ?>
-
-            <tr>
-                <th><?= $bk["id_buku"]; ?></th>
-                <td><?= $bk["judul"]; ?></td>
-                <th><?= $bk["tahun"]; ?></th>
-                <td><?= $bk["penulis"]; ?></td>
-                <th><?= $bk["rak"]; ?></th>
-                <td><?= $bk["genre"]; ?></td>
-                <th><?= $bk["status"]; ?></th>
-                <th>
-                    <a href="../update/updateBuku.php?id=<?= $bk["id_buku"]; ?>">
-                        <img src="../icon/update.png" style="text-align: center;" alt="Qries" width="30">
-                    </a>
-                </th>
-                <th>
-                    <a href="../delete/deleteBuku.php?id=<?= $bk["id_buku"]; ?>">
-                        <img src="../icon/delete.png" style="text-align: center;" alt="Qries" width="30">
-                    </a>
-                </th>
-            </tr>
+            <?php foreach ($buku as $bk) : ?>
+                <tr>
+                    <th><?= $bk["id_buku"]; ?></th>
+                    <td><?= $bk["judul"]; ?></td>
+                    <th><?= $bk["tahun"]; ?></th>
+                    <td><?= $bk["penulis"]; ?></td>
+                    <th><?= $bk["rak"]; ?></th>
+                    <td><?= $bk["genre"]; ?></td>
+                    <th><?= $bk["status"]; ?></th>
+                    <th>
+                        <img src="../img-barcode/<?= $bk["barcode"]; ?>">
+                        <a href="printBarcode.php?kode=<?= $bk["barcode"]; ?>">
+                            <img src="../icon/printer.png" width="30" style="padding-bottom: 30px; padding-left: 10px;">
+                        </a>
+                    </th>
+                    <th>
+                        <a href="../update/updateBuku.php?id=<?= $bk["id_buku"]; ?>">
+                            <img src="../icon/update.png" style="text-align: center;" alt="Qries" width="30">
+                        </a>
+                    </th>
+                    <th>
+                        <a href="../delete/deleteBuku.php?id=<?= $bk["id_buku"]; ?>">
+                            <img src="../icon/delete.png" style="text-align: center;" alt="Qries" width="30">
+                        </a>
+                    </th>
+                </tr>
             <?php endforeach; ?>
         </tbody>
-    </table>
-<?php endif; ?>
+        </table>
+    <?php endif; ?>
