b0b508d238b9fdf24ada35f5972440611a021fd5
Imam Maskuri <90993075+mskur@users.noreply.github.com>
2022-05-18 18:26:13 +0700

    Delete script.js

--- script/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-    $('#keyword').on('keyup',function(){
-        $.get('../script/search.php?keyword='+$('#keyword').val(),function(data){
-            $('#tabelData').html(data);
-            $('.').hide();
-        });
-    });
-})
